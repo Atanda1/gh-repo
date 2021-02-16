632728b92d04e0776e51d16ae764c7647b524343
David Atanda <atandadave@gmail.com>
2021-02-16 20:29:34 +0100

    Github clone done

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import MyRepo from "../views/MyRepo.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Repo",
+    component: MyRepo
+  }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router;
